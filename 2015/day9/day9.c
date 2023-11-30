fcdf100f597eb2d86700ab99bbeba1a215c1d965
maddyakhil <akhil.maddineni2@gmail.com>
2023-11-30 13:07:00 +0530

    incomplete day 9

---
+++ 2015/day9/day9.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp = fopen("input.txt","r");
+    if(NULL == fp)
+    {
+        printf("Error in opening the file \n");
+        exit(1);
+    }
+    char line[100];
+
+    while(fgets(line,sizeof(line),fp))
+    {
+        //TODO
+    }
+
+}
