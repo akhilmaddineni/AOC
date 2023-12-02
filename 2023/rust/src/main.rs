45c6d688de7504cbc1c74b4d85de530d07500605
maddyakhil <akhil.maddineni2@gmail.com>
2023-12-02 21:44:16 +0530

    implement cmd line input with day number

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,5 +1,16 @@
+use std::env;
+
 mod day1;
 
 fn main() {
-    day1::solve();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: cargo run [day_num]");
+        return;
+    }
+    match args[1].as_str() {
+        "1" => day1::solve(),
+        _=>println!("{} day not implemented",args[1].as_str()),
+    }
 }
