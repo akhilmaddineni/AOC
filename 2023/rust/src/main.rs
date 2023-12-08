9ceed1f8f2ddbcad174b0118afac6301ee5ced8b
maddyakhil <akhil.maddineni2@gmail.com>
2023-12-08 08:24:34 +0530

    config main

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -4,6 +4,8 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
+mod day6;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,6 +19,8 @@ fn main() {
         "2" => day2::solve(),
         "3" => day3::solve(),
         "4" => day4::solve(),
+        "5" => day5::solve(),
+        "6" => day6::solve(),
         _=>println!("day {} not implemented",args[1].as_str()),
     }
 }
