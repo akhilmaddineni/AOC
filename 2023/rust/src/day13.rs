f988767bcb02b02cb43e8bcf2fa7d716fca083cd
maddyakhil <akhil.maddineni2@gmail.com>
2023-12-15 22:56:15 +0530

    day 13 part 1

--- 2023/rust/src/day13.rs
+++ 2023/rust/src/day13.rs
@@ -19,98 +19,86 @@ fn solve_part1(matrix_vector: &Vec<Vec<Vec<char>>> ) {
         let mut found_reflection: bool = false ; 
         let mut row_idx_reflection = 0; 
         let mut col_idx_reflection = 0; 
-
-        //check row wise reflection 
-        // if (num_rows%2 == 0) {
-        //     let mid = 
-        // }
-        //check col reflection 
-        // if num_rows%2 == 0 {
-        //     (found_reflection,row_idx_reflection) = check_row_reflection(&matrix,0); 
-        // }
-        // else {
-            // for i in 0..
-            // (found_reflection,row_idx_reflection) = check_row_reflection(&matrix,0) ; 
-            // if !found_reflection {
-            //     (found_reflection,row_idx_reflection) = check_row_reflection(&matrix,1); 
-            // }
-        // }
-        println!("for matrix : {:?} found reflection : {}",matrix,found_reflection);
+        for col_idx in 1..matrix[0].len() {
+            (found_reflection,col_idx_reflection) = check_col_reflection(&matrix,col_idx);
+            if found_reflection {
+                println!("found col reflection at test {}",col_idx_reflection);
+                break;
+            }
+        }
+        if found_reflection {
+            ans += col_idx_reflection as u32;
+            continue;
+        }
+        for row_idx in 1..matrix.len() {
+            (found_reflection,row_idx_reflection) = check_row_reflection(&matrix,row_idx);
+            if found_reflection {
+                println!("found row reflection at test {}",row_idx_reflection);
+                break;
+            }
+        }
         if found_reflection {
             ans += (row_idx_reflection as u32)*100;
             continue;
         }
-
-        if (num_cols%2 == 0) {
-            (found_reflection,col_idx_reflection) = check_col_reflection(&matrix,0);
-        }
-        else {
-            println!("checking ß");
-            (found_reflection,col_idx_reflection) = check_col_reflection(&matrix,0) ; 
-            if !found_reflection {
-                (found_reflection,col_idx_reflection) = check_col_reflection(&matrix,1); 
-            }
-        }
-        println!("for matrix : {:?} found reflection : {}",matrix,found_reflection);
-        if found_reflection {
-            ans += col_idx_reflection as u32;
-        }
-        println!("no reflection found"); 
+        println!("no reflection!");
         std::process::exit(1);
     }
     println!("part 1 ans : {}",ans);
 }
 
 pub fn check_col_reflection(matrix: &Vec<Vec<char>>,col_start: usize)->(bool,usize) {
-    println!("checking col : ");
-    let mut len_cols: usize = 0;
-    if col_start == 0 && matrix[0].len()%2 == 1 {
-        len_cols = matrix[0].len()-1;
+    //find start end and mid is col_start
+    let mut start: usize = 0;
+    let mut end: usize = matrix[0].len(); 
+    //println!("inital col start : {} , start {} end {}",col_start,start,end);
+    if col_start < end-col_start {
+        end = 2*col_start ; 
     }
     else {
-        len_cols = matrix[0].len();
+        start = 2*col_start-end;
     }
-    let mid_col: usize = (len_cols/2);
+    //println!("col start : {} , start {} end {}",col_start,start,end);
     for i in 0..matrix.len() {
-        for j in col_start..col_start+mid_col{
-            if matrix[i][j] != matrix[i][len_cols - j - 1+col_start] {
-                println!("idx {}{} {}   ref{} {}",i,j,matrix[i][j],len_cols - j - 1, matrix[i][len_cols - j - 1]);
+        for j in start..col_start{
+            //println!("process row : {:?} idx : {}",matrix[i],i);
+            if matrix[i][j] != matrix[i][end - j - 1+start] {
+                //println!("idx {}{} {}   ref{} {}",i,j,matrix[i][j],end - j - 1+start, matrix[i][end - j - 1+start]);
                 return (false,0)
             }
         }
     }
-    return (true,mid_col+col_start)
+    return (true,col_start)
 }
 
 pub fn check_row_reflection(matrix: &Vec<Vec<char>>,row_start: usize)->(bool,usize) {
-    println!("checking row");
-    let mut len_rows: usize = 0;
-    if row_start == 0 && matrix.len()%2 == 1 {
-        len_rows = matrix.len()-1;
+    //println!("checking row");
+    let mut start: usize = 0;
+    let mut end: usize = matrix.len(); 
+    //println!("inital row start : {} , start {} end {}",row_start,start,end);
+    if row_start < end-row_start {
+        end = 2*row_start ; 
     }
     else {
-        len_rows = matrix.len();
+        start = 2*row_start-end;
     }
-    println!("num_rows : {}",len_rows);
-    let mid_row: usize = len_rows/2;
+    //println!("row start : {} , start {} end {}",row_start,start,end);
     for j in 0..matrix[0].len() {
-        for i in row_start..row_start+mid_row{
+        for i in start..row_start{
             //println!("process row : {:?} idx : {}",matrix[i],i);
-            if matrix[i][j] != matrix[len_rows-i-1+row_start][j] {
-                println!("idx {}{} {}   ref{} {}",i,j,matrix[i][j],len_rows-i-1+row_start, matrix[len_rows-i-1+row_start][j]) ;
+            if matrix[i][j] != matrix[end-i-1+start][j] {
+                //println!("idx {}{} {}   ref{} {}",i,j,matrix[i][j],end-i-1+start, matrix[end-i-1+start][j]) ;
                 return (false,0)
             }
         }
     }
-    return (true,mid_row+row_start)
+    return (true,row_start)
 }
 
 
 pub fn solve() {
     let input = read_input("input/input_day13.txt");
     let input_lines: Vec<String> = parse_lines_to_vec(&input);
-    // solve_part1(&input_lines);
-    // solve_part2(&input_lines);
     let mut matrix_vector: Vec<Vec<Vec<char>>> = Vec::new(); 
     let mut i = 0;
     let mut temp_matrix: Vec<Vec<char>> = Vec::new();
