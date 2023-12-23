588870ec4a01f9db45bc9ec98445fbc92040586c
maddyakhil <akhil.maddineni2@gmail.com>
2023-12-23 18:59:04 +0530

    handle io

--- 2023/rust/src/day12.rs
+++ 2023/rust/src/day12.rs
@@ -10,8 +10,20 @@ fn parse_lines_to_vec(input: &str) -> Vec<String> {
 
 fn solve_part1(input_lines: &Vec<String>) {
     let mut ans: u64 = 0;
-    let mut str_arr: Vec<&str> = input_lines[0].split_whitespace().collect(); 
-    println!("{:?}",str_arr);
+    for input_line in input_lines{
+        let mut str_arr: Vec<&str> = input_line.split_whitespace().collect(); 
+        println!("{:?}",str_arr);
+        let mut char_array: Vec<_> = str_arr[0].chars().collect(); 
+        let mut seq: Vec<u16> = Vec::new();
+        let mut temp_str: Vec<_> = str_arr[1].split(",").collect();
+        for ele in temp_str {
+            seq.push(ele.parse::<u16>().unwrap());
+        }
+        println!("{:?}",char_array);
+        println!("{:?}",seq);
+        ans += solve(&char_array,&seq,0,0);
+    }
+    
 
 }
 
